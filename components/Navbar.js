2cbd14dc96eac42d4a725cdacc39ecd54244f8a7
cocotran <anhquangtran2703@gmail.com>
2021-04-12 03:44:23 -0400

    Added gameboy font

---
+++ components/Navbar.js
@@ -0,0 +1,17 @@
+export default function Navbar() {
+    return (
+        <>
+
+
+            <nav className="py-3 px-8 flex justify-end items-center text-gray-100">
+
+                <button>Skills</button>
+                <button>Projects</button>
+                <button>Contact</button>
+
+            </nav>
+
+
+        </>
+    )
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,19 @@
 import Head from 'next/head'
 
+import Navbar from '../components/Navbar' 
+
+
 export default function Home() {
   return (
-    <div className="text-red-900">
-      Hello
+    <>
+
+    <div className="app bg-gray-900">
+      
+      <Navbar />
+
+      <div className="text-white text-2xl">CHIM TOOOOO</div>
+
     </div>
+    </>
   )
 }
